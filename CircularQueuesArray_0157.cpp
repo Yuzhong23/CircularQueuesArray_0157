4138bca6e1f73c111ea9a5a46f9b00650067d964
Yuzhong23 <xixixixifatur@gmail.com>
2024-05-30 10:39:10 +0700

    mengecek apakah antrian kosong

--- CircularQueuesArray_0157.cpp
+++ CircularQueuesArray_0157.cpp
@@ -19,11 +19,20 @@ public:
         cin >> num;
         cout << endl;
 
+
         //cek apakah antrian penuh
-        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
             cout << "\nQueue overflow\n";
-        return;
+            return;
+        }
+
+        //cek apakah antrian kosong
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
     }
+    
 
 
 
