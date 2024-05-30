af6b509bf1c27554577e08d2d2997a617e12c821
Yuzhong23 <xixixixifatur@gmail.com>
2024-05-30 10:30:29 +0700

    Membuat void insert

--- CircularQueuesArray_0157.cpp
+++ CircularQueuesArray_0157.cpp
@@ -6,11 +6,18 @@ using namespace std;
 class Queues {
     int FRONT, REAR, max = 5;
     int queue_array[5];
+
 public:
     Queues() {
         FRONT = -1;
         REAR = -1;
     }
+
+    void insert() {
+        int num;
+        cout << "Enter a number: ";
+    }
+
 };
 
 int main()
